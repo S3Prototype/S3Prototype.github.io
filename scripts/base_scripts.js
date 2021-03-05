9441f7271cef51d9e4d122b4c9921f25631b673a
S3Prototype <liuqahs15@gmail.com>
2021-03-04 19:28:38 -0500

    Fixed site title.

--- scripts/base_scripts.js
+++ scripts/base_scripts.js
@@ -3,7 +3,9 @@
 document.querySelectorAll('summary.project-title').forEach(clickedSummary=>{
     clickedSummary.onclick = ()=>{
         document.querySelectorAll('summary.project-title').forEach(otherSummary=>{
-            if(otherSummary.classList !== clickedSummary.classList && otherSummary.parentElement.open){                
+            if(otherSummary.classList !== clickedSummary.classList
+                && otherSummary.parentElement.open){                
+                    
                 otherSummary.parentElement.querySelectorAll('details')
                     .forEach(detail=>{detail.open = false})
                 otherSummary.parentElement.open = false
